b726fc0c6be2bc4be5a10716113107465e341515
Vyshniakov Ihor <gramazekavip@gmail.com>
2021-11-25 21:31:38 +0200

    added: table pagination, fixed issue with typegraphy in tablehead, dialogue with answers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { AppBar, Container, Divider, List, ListItem, ListItemText, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Typography } from '@mui/material'
+import { AppBar, Button, Container, Dialog, DialogContent, DialogContentText, DialogTitle, Paper, Table, TableBody, TableCell, TableContainer, TablePagination, TableRow, Typography, List, ListItem, ListItemText, Divider } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 
 import './App.css';
+import { Box } from '@mui/system';
 
 function getQuestions(page, questionsQuantity) {
 	return axios.get(`https://api.stackexchange.com/2.3/questions?page=${page}&pagesize=${questionsQuantity}&order=desc&sort=activity&site=stackoverflow`)
@@ -31,10 +33,11 @@ const App = () => {
 	const [isModalOpen, setModalOpen] = useState(false)
 	const [actualAnswers, setActualAnswers] = useState([])
 	const [questions, setQuestions] = useState([])
-	console.log(questions)
 
 	const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+	console.log(questions)
 
 	const changePage = (event, newPage) => {
     setPage(newPage);
@@ -51,27 +54,27 @@ const App = () => {
 	}
 
 	useEffect(() => {
-		console.log('page', page)
 		getQuestions(page + 1, rowsPerPage).then(questions => setQuestions(questions))
 	}, [page, rowsPerPage])
 
   return (
     <div className="App">
 			<AppBar position="static">
-				<Typography variant="h6" component="h1" align='center' sx={{height: '40px', lineHeight: '40px'}}>
+				<Typography
+					variant="h6"
+					component="h1"
+					align='center'
+					sx={{height: '40px', lineHeight: '40px'}}
+				>
 					Stack overflow QnA App
 				</Typography>
 			</AppBar>
 			<Container sx={{width: '100%', p: '40px 20px 80px'}}>
+				<Typography variant="h5" align='center' sx={{mb: '10px'}}>
+					Questions
+				</Typography>
 				<TableContainer component={Paper}>
 					<Table>
-						<TableHead>
-							<TableRow>
-								<Typography variant="h5" align='center'>
-									Questions
-								</Typography>
-							</TableRow>
-						</TableHead>
 						<TableBody>
 							{questions.map(question => (
 								<TableRow
@@ -85,7 +88,7 @@ const App = () => {
 							))}
 							<TableRow>
 								<TablePagination
-									count={100}
+									count={120}
 									page={page}
 									onPageChange={changePage}
 									rowsPerPage={rowsPerPage}
@@ -98,29 +101,65 @@ const App = () => {
 						</TableBody>
 					</Table>
 				</TableContainer>
-				{
-					isModalOpen && (
-						<div className="modal">
-							<div className="list">
-								<ul>
-									<div className="wrap">
-										{actualAnswers.map(answer => (
-											<li>
-												{answer.body}
-												<div className="details">
-													<p className="date">{convertTimestampToDateString(answer.creation_date)}</p>
-													<p className='author'>{answer.owner.display_name}</p>
-												</div>
-											</li>
-											))}
-									</div>
-									{actualAnswers.length == 0 && (<p className='info'>There's no answers to this question.</p>)}
-								</ul>
-								<p className="cross" onClick={()=>setModalOpen(false)}>X</p>
-							</div>
-						</div>
-					)
-				}
+				<Dialog
+					scroll='paper'
+					open={isModalOpen}
+					onClose={()=>setModalOpen(false)}
+				>
+					<DialogTitle
+						variant="h5"
+						component="div"
+						sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}
+					>
+						Answers
+						<Button onClick={()=>setModalOpen(false)}>
+							<CloseIcon/>
+						</Button>
+					</DialogTitle>
+					<DialogContent dividers={true}>
+						<DialogContentText>
+							<List>
+								{actualAnswers.map(answer => (
+									<>
+										<ListItem
+											component='div'
+											sx={{display: 'flex', flexDirection: 'column'}}
+										>
+											<ListItemText primary={answer.body} />
+											<Box
+												sx={{width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}
+											>
+												<Typography
+													component='div'
+													variant='subtitle2'
+													sx={{color: 'red'}}
+												>
+													{convertTimestampToDateString(answer.creation_date)}
+												</Typography>
+												<Typography
+													component='div'
+													variant='h6'
+													sx={{color: 'blue'}}
+												>
+													{answer.owner.display_name}
+												</Typography>
+											</Box>
+										</ListItem>
+										<Divider/>
+									</>
+								))}
+								{actualAnswers.length == 0 && (
+									<>
+										<ListItem>
+											<ListItemText primary="There's no answers to this question."/>
+										</ListItem>
+										<Divider/>
+									</>
+								)}
+							</List>
+						</DialogContentText>
+					</DialogContent>
+				</Dialog>
 			</Container>
     </div>
   );
